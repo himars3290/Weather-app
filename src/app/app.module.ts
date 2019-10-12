1b91bfe26c862b4b83d45e9da6a2250178cba5b2
rashmi <rashmi.manandhar7@gmail.com>
2019-10-12 19:44:15 -0400

    create a component to add the city

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { WeatherCardComponent } from './components/weather-card/weather-card.component';
+import { AddCardComponent } from './components/add-card/add-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    WeatherCardComponent
+    WeatherCardComponent,
+    AddCardComponent
   ],
   imports: [
     BrowserModule,
