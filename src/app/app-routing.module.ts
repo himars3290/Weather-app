dfc457dabb9464766d208f1b615df812812d7f3c
rashmi <rashmi.manandhar7@gmail.com>
2019-10-12 20:19:32 -0400

    add all routes to app routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import {DetailsComponent} from './components/details/details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'details/:city', component: DetailsComponent},
+  // {path: 'add', component: AddComponent},
+  // {path: 'login', component: LoginComponent},
+  // {path: 'signup', component: SignupComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
